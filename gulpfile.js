654fb9ff18a226a60726402219758a08d9103ab7
wheats <532770034@qq.com>
2017-05-06 09:36:40 +0800

    add files

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ const babel = require('gulp-babel');
 const gulpBrowser = require("gulp-browser");
 const header = require('gulp-header');
 const uglify = require('gulp-uglify');
+const minifyCSS  = require('gulp-minify-css');
+const rename = require('gulp-rename');
+const notify = require('gulp-notify');
 
 var pkg = require('./package.json');
 var banner = [
@@ -15,18 +18,33 @@ var banner = [
   ''
 ].join('\n');
 
-gulp.src('./foo/*.js')
-    .pipe(header(banner, {pkg : pkg}))
-    .pipe(gulp.dest('./dist/'))
+gulp.task('js-uglify', () => {
+  return gulp.src('static/javascripts/src/**/*.js')
+      .pipe(gulpBrowser.browserify([
+        {transform : "babelify", options : {presets : [ "es2015" ]}}
+      ]))
+      .pipe(uglify())
+      .pipe(header(banner, {pkg : pkg}))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('static/javascripts/dist'))
+      .pipe(notify({ message: 'js 编译完成！' }));
+});
 
-        gulp.task('es6-es5', () => {
-          return gulp.src('static/javascripts/src/**/*.js')
-              .pipe(gulpBrowser.browserify([
-                {transform : "babelify", options : {presets : [ "es2015" ]}}
-              ]))
-              .pipe(uglify())
-              .pipe(header(banner, {pkg : pkg}))
-              .pipe(gulp.dest('static/javascripts/dist'));
-        });
+gulp.task('css-minify', ()=> {
+  return gulp.src('static/css/src/**/*.css')
+      .pipe(minifyCSS())
+      .pipe(header(banner, {pkg : pkg}))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('static/css/dist'))
+      .pipe(notify({ message: 'css  编译完成！' }));
+});
 
-gulp.task('default', [ 'es6-es5' ]);
+gulp.task('watch-js', ()=> {
+  gulp.watch('static/javascripts/src/**/*.js', [ 'js-uglify' ]);
+});
+
+gulp.task('watch-css', ()=> {
+  gulp.watch('static/css/src/**/*.css', [ 'css-minify' ]);
+});
+
+gulp.task('default', ['js-uglify', 'css-minify', 'watch-js', 'watch-css']);
