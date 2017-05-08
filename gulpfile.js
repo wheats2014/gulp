3b7a0197d81e04c1dbcdf6e4340171680c1a9d00
wheats2014 <532770034@qq.com>
2017-05-08 10:46:06 +0800

    Modify file

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,17 @@ const babel = require('gulp-babel');
 const gulpBrowser = require("gulp-browser");
 const header = require('gulp-header');
 const uglify = require('gulp-uglify');
-const minifyCSS  = require('gulp-minify-css');
+const less = require('gulp-less');
+
+// 添加浏览器前缀
+const autoprefixer = require('gulp-autoprefixer');
+
+const minifyCSS = require('gulp-minify-css');
 const rename = require('gulp-rename');
 const notify = require('gulp-notify');
 
-var pkg = require('./package.json');
-var banner = [
+let pkg = require('./package.json');
+let banner = [
   '/**',
   ' * <%= pkg.name %> - <%= pkg.description %>',
   ' * @version v<%= pkg.version %>',
@@ -20,31 +25,36 @@ var banner = [
 
 gulp.task('js-uglify', () => {
   return gulp.src('static/javascripts/src/**/*.js')
-      .pipe(gulpBrowser.browserify([
-        {transform : "babelify", options : {presets : [ "es2015" ]}}
-      ]))
+      .pipe(gulpBrowser.browserify(
+          [ {transform : "babelify", options : {presets : [ "es2015" ]}} ]))
       .pipe(uglify())
       .pipe(header(banner, {pkg : pkg}))
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('static/javascripts/dist'))
-      .pipe(notify({ message: 'js 编译完成！' }));
+      .pipe(rename({suffix : '.min'}))
+      .pipe(gulp.dest('static/javascripts/dist'));
 });
 
-gulp.task('css-minify', ()=> {
+gulp.task('less', () => {
+  return gulp.src('static/less/**/*.less')
+      .pipe(less())
+      .pipe(gulp.dest('static/css/src'));
+});
+
+gulp.task('css-minify', () => {
   return gulp.src('static/css/src/**/*.css')
+      .pipe(autoprefixer({browsers : [ 'last 5 versions' ], cascade : false}))
       .pipe(minifyCSS())
       .pipe(header(banner, {pkg : pkg}))
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('static/css/dist'))
-      .pipe(notify({ message: 'css  编译完成！' }));
+      .pipe(rename({suffix : '.min'}))
+      .pipe(gulp.dest('static/css/dist'));
+  //.pipe(notify({message : 'css  编译完成！'}));
 });
+//
+// gulp.task('watch-js', () => {
+//   gulp.watch('static/javascripts/src/**/*.js', [ 'js-uglify' ]);
+// });
+//
+// gulp.task('watch-css',
+//           () => { gulp.watch('static/css/src/**/*.css', [ 'css-minify' ]);
+//           });
 
-gulp.task('watch-js', ()=> {
-  gulp.watch('static/javascripts/src/**/*.js', [ 'js-uglify' ]);
-});
-
-gulp.task('watch-css', ()=> {
-  gulp.watch('static/css/src/**/*.css', [ 'css-minify' ]);
-});
-
-gulp.task('default', ['js-uglify', 'css-minify', 'watch-js', 'watch-css']);
+gulp.task('default', [ 'less', 'css-minify' ]);
